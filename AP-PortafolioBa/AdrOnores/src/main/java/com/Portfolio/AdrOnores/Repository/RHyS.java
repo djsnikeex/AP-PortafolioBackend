94d58eff188348461c4c24f38050a9632170c70b
djsnikeex <nicolasonores@hotmail.com>
2023-03-24 12:40:38 -0300

    Backend- HYS

---
+++ AP-PortafolioBa/AdrOnores/src/main/java/com/Portfolio/AdrOnores/Repository/RHyS.java
@@ -0,0 +1,12 @@
+
+package com.Portfolio.AdrOnores.Repository;
+
+import com.Portfolio.AdrOnores.Entity.HyS;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RHyS extends JpaRepository<HyS, Integer>{
+    Optional<HyS> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+}
---
+++ AP-PortafolioBa/AdrOnores/src/main/java/com/Portfolio/AdrOnores/Service/SHyS.java
@@ -0,0 +1,44 @@
+package com.Portfolio.AdrOnores.Service;
+
+import com.Portfolio.AdrOnores.Entity.HyS;
+import com.Portfolio.AdrOnores.Repository.RHyS;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Transactional
+@Service
+public class SHyS {
+    @Autowired
+    RHyS rHyS;
+    
+    public List<HyS> list(){
+        return rHyS.findAll();
+    }
+    
+    public Optional<HyS> getOne(int id){
+        return rHyS.findById(id);
+    }
+    
+    public Optional<HyS> getByNombre(String nombre){
+        return rHyS.findByNombre(nombre);
+    }
+    
+    public void save(HyS skill){
+        rHyS.save(skill);
+    }
+    
+    public void delete(int id){
+        rHyS.deleteById(id);
+    }
+    
+    public boolean existsById(int id){
+        return rHyS.existsById(id);
+    }
+    
+    public boolean existsByNombre(String nombre){
+        return rHyS.existsByNombre(nombre);
+    }
+}
